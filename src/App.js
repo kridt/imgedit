780e6d020aaf02dd9e2512a81b568aa2954bc8b5
Christian <chrnielsen2003@gmail.com>
2022-08-26 22:13:42 +0200

    .

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   const [img, setImg] = useState("https://via.placeholder.com/300");
   const [imgHeight, setImgHeight] = useState(300);
   const [imgWidth, setImgWidth] = useState(300);
+  const [blackNwhite, setBlackNwhite] = useState(true);
+  const [contrast, setContrast] = useState(100);
+  const [light, setLight] = useState(100);
+  const [saturate, setSaturate] = useState(100);
 
   function chosenImg(e) {
     setImg(URL.createObjectURL(e.target.files[0]));
@@ -16,7 +20,7 @@ function App() {
 
   return (
     <>
-      <h1>hello</h1>
+      <h1 style={{ textAlign: "center" }}>Billed redigering</h1>
 
       <div>
         <input
@@ -26,7 +30,56 @@ function App() {
         />
       </div>
 
-      <p>Dit redigerede billede</p>
+      <div className="filterOptions">
+        <div>
+          <label>Sort Hvid?</label>
+          <input
+            defaultChecked
+            onClick={() => setBlackNwhite(!blackNwhite)}
+            type="checkbox"
+            name="blackNwhite"
+            id="blackNwhite"
+          />
+        </div>
+        <div>
+          <label>Kontrast? {contrast}%</label>
+          <input
+            onChange={(e) => setContrast(e.target.value)}
+            type="range"
+            min={0}
+            defaultValue={100}
+            max={200}
+            name="contrast"
+            id="contrast"
+          />
+        </div>
+        <div>
+          <label>lys i billed? {light}%</label>
+          <input
+            onChange={(e) => setLight(e.target.value)}
+            type="range"
+            min={0}
+            defaultValue={100}
+            max={200}
+            name="light"
+            id="light"
+          />
+        </div>
+        <div>
+          <label>billed mætte? {saturate}%</label>
+          <input
+            onChange={(e) => setSaturate(e.target.value)}
+            type="range"
+            min={0}
+            defaultValue={100}
+            max={200}
+            name="saturate"
+            id="saturate"
+          />
+        </div>
+      </div>
+
+      <p style={{ textAlign: "center" }}>Dit redigerede billede</p>
 
       <div className="canvas">
         <div className="grid">
@@ -50,12 +103,31 @@ function App() {
         </div>
 
         <div className="img">
-          <img
-            style={{ width: "300px", filter: "grayscale(100%)" }}
-            src={img}
-            alt="pic"
-            id="theImg"
-          />
+          {blackNwhite ? (
+            <>
+              <img
+                style={{
+                  width: "300px",
+                  filter: `grayscale(100%) contrast(${contrast}%) brightness(${light}%) saturate(${saturate}%)`,
+                }}
+                src={img}
+                alt="pic"
+                id="theImg"
+              />
+            </>
+          ) : (
+            <>
+              <img
+                style={{
+                  width: "300px",
+                  filter: `grayscale(0%) contrast(${contrast}%) brightness(${light}%) saturate(${saturate}%)`,
+                }}
+                src={img}
+                alt="pic"
+                id="theImg"
+              />
+            </>
+          )}
         </div>
       </div>
     </>
